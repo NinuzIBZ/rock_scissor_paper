eb1f9ad9546a69319091627bbf9c4893c93901c1
NinuzIBZ <antonino.dellalb@gmail.com>
2022-11-14 20:19:50 +0100

    added getComputerChoice function

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const options = ['rock','scissor','paper']
 
 let playerSelection = 
 let computerSelection = 
+
+function getComputerChoice() {
+    let computerChoice = options[Math.floor(Math.random() * options.length)]
+    return computerChoice
+}
