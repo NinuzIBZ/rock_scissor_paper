0c32f4da163429e2618d7e1b7b74fd58c739bb0f
NinuzIBZ <antonino.dellalb@gmail.com>
2022-11-14 20:21:02 +0100

    added playerInput function

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ function getComputerChoice() {
     let computerChoice = options[Math.floor(Math.random() * options.length)]
     return computerChoice
 }
+
+function playerInput() {
+    let playerChoice = prompt('Rock,Scissor or Paper?')
+    return playerChoice.toLowerCase()
+}
+
