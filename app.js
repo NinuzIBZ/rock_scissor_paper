19ff1599e7dbb68ccb7bedcd95c2678a8cef95fc
NinuzIBZ <antonino.dellalb@gmail.com>
2022-11-14 21:38:41 +0100

    Fixed prompt been called 2 times and code cleanup

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const options = ['rock','scissor','paper']
 
-let playerSelection = playerInput();
-let computerSelection = getComputerChoice();
-
 function getComputerChoice() {
     let computerChoice = options[Math.floor(Math.random() * options.length)]
     return computerChoice
@@ -14,36 +11,46 @@ function playerInput() {
 }
 
 function playRound(playerSelection,computerSelection) {
+    let playerSelection = playerInput();
+    let computerSelection = getComputerChoice();
+
     if(computerSelection == playerSelection) {
-        return alert(`Computer had ${computerSelection} & you had ${playerSelection}. It's a draw!`)
-    }
-    else if(computerSelection == 'rock' && playerSelection == 'scissor' || computerSelection == 'scissor' && playerSelection == 'paper' || computerSelection == 'paper' && playerSelection == 'rock' ) {
-        return alert(`Computer had ${computerSelection} & you had ${playerSelection}. You lost!`)
-    }
-    else if(playerSelection == 'rock' && computerSelection == 'scissor' || playerSelection == 'scissor' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'rock' ) {
-        return alert(`Computer had ${computerSelection} & you had ${playerSelection}. You win!`)
+        alert(`Computer had ${computerSelection} & you had ${playerSelection}. It's a draw!`)
+        return;
+    } else if(
+        (computerSelection == 'rock' && playerSelection == 'scissor') || 
+        (computerSelection == 'scissor' && playerSelection == 'paper') || 
+        (computerSelection == 'paper' && playerSelection == 'rock') 
+    ) {
+        alert(`Computer had ${computerSelection} & you had ${playerSelection}. You lost!`);
+        return 'computerScore';
+    } else if (
+        (playerSelection == 'rock' && computerSelection == 'scissor') || 
+        (playerSelection == 'scissor' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') 
+    ) {
+        alert(`Computer had ${computerSelection} & you had ${playerSelection}. You win!`);
+        return 'playerScore';
     }
 
     alert('You wrote some bullshit nub!')
 }
 
-//playRound()
-
 function game() {
     let playerScore = 0
     let computerScore = 0
+
     for(let i = 0; i < 5; i++) {
         const computerSelection = getComputerChoice()
         const playerSelection = playerInput()
+
         let winner = playRound(playerSelection,computerSelection)
+
         if(winner === 'playerScore') {
             playerScore++
         }
         else if(winner === 'computerScore') {
             computerScore++
-        }
-        else {
-            i
         }
     }
 
@@ -52,13 +59,13 @@ function game() {
 
 function winner(playerScore,computerScore) {
     if(playerScore > computerScore) {
-        alert('You Win!')
-    }
-    else {
+        alert('You Win!');
+    } else if (computerScore > playerScore) {
         alert('You Lost!')
+    } else {
+        alert('Draw!')
     }
 }
 
 game();
-winner();
 
