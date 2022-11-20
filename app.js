2103cc6fb16291cde9c31168736f680e19a8c827
NinuzIBZ <antonino.dellalb@gmail.com>
2022-11-20 02:41:01 +0100

    Added round counting plus tie alert

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const rock = document.querySelector('.rock');
 const scissor = document.querySelector('.scissor');
 const paper = document.querySelector('.paper');
-
-rock.addEventListener('click', function() {
-    console.log('rock')
-});
-
-
-
-
+const computer = document.querySelector('.comp');
+const container = document.querySelector('.container')
+const pScore = document.querySelector('.player-score');
+const cScore = document.querySelector('.computer-score');
 
 const options = ['rock','scissor','paper']
 
@@ -17,33 +13,52 @@ function getComputerChoice() {
     return computerChoice
 }
 
-function playerInput() {
-    let playerChoice = prompt('Rock,Scissor or Paper?')
-    return playerChoice.toLowerCase()
-}
+rock.addEventListener('click', function() {
+    playRound('rock',getComputerChoice());
+    console.log(playRound('rock'),getComputerChoice())
+});
+
+scissor.addEventListener('click', function() {
+    playRound('scissor',getComputerChoice());
+    console.log(playRound(),getComputerChoice())
+});
+
+paper.addEventListener('click', function() {
+    playRound('paper',getComputerChoice());
+    console.log(playRound(),getComputerChoice())
+});
+
+computer.addEventListener('click', function() {
+    alert('I do my own things! Dont you worry!')
+})
 
 function playRound(playerSelection,computerSelection) {
 
+    let playerScore = parseInt(document.querySelector('.player-score').innerHTML);
+    let computerScore = parseInt(document.querySelector('.computer-score').innerHTML);
+
     if(computerSelection == playerSelection) {
-        alert(`Computer had ${computerSelection} & you had ${playerSelection}. It's a draw!`)
-        return;
+        roundWinner = 'tie';
+        alert('Was a Tie! Keep going!')
     } else if(
         (computerSelection == 'rock' && playerSelection == 'scissor') || 
         (computerSelection == 'scissor' && playerSelection == 'paper') || 
         (computerSelection == 'paper' && playerSelection == 'rock') 
     ) {
-        alert(`Computer had ${computerSelection} & you had ${playerSelection}. You lost!`);
-        return 'computerScore';
+        //display the result on the div.
+        computerScore++;
+        cScore.innerText = computerScore;
+        roundWinner = 'computer';
     } else if (
         (playerSelection == 'rock' && computerSelection == 'scissor') || 
         (playerSelection == 'scissor' && computerSelection == 'paper') ||
         (playerSelection == 'paper' && computerSelection == 'rock') 
     ) {
-        alert(`Computer had ${computerSelection} & you had ${playerSelection}. You win!`);
-        return 'playerScore';
+        //display the result on the div.
+        playerScore++;
+        pScore.innerText = playerScore;
+        roundWinner = 'player';
     }
-
-    alert('You wrote some bullshit nub!')
 }
 
 function game() {
